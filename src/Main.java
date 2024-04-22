0028621f57d23317f075f62e85b2b8f18c3fac73
Egor Girko <e.girko@ya.ru>
2024-04-23 00:20:11 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 0;
-        int y = n + 3;
+
+
+
+
     }
 }
 
---
+++ src/Window.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Window extends JFrame {
+
+    public Window(){
+        super("My something");
+        ;
+    }
+}
